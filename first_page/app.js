4cce3f26dfaa1071ae53618a2ff087132807e98b
Shreya shukla <shreyaa1@users.noreply.github.com>
2017-08-30 16:19:09 +0530

    Add files via upload

---
+++ first_page/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
